de186df85afb552f34669cf21fda4ae278bfd861
klstxii <kiyoharusarmiento@gmail.com>
2024-11-10 22:32:49 +0800

    feat: animated bg

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ expenseForm.addEventListener('submit', (event) => {
         totalExpense += expenseAmount;
         const newExpenseRow = document.createElement('tr');
         newExpenseRow.innerHTML = `
-            <td>${expenseName}</td>
-            <td>PHP ${expenseAmount}</td>
-            <td>${expenseDate}</td>
-            <td><button onclick="removeExpense()" tabindex="0">Remove</button></td>
+            <td style="background-color: transparent;">${expenseName}</td>
+            <td style="background-color: transparent;">PHP ${expenseAmount}</td>
+            <td style="background-color: transparent;">${expenseDate}</td>
+            <td style="background-color: transparent;"><button onclick="removeExpense()" tabindex="0">Remove</button></td>
         `;
         expenseTable.appendChild(newExpenseRow);
         totalExpenseElement.textContent = `${totalExpense}`;
@@ -46,3 +46,31 @@ function removeExpense(expenseRow) {
         expenseTable.style.display = 'none';
     }
 }
+
+function createMovingShape() {
+    const shapes = document.querySelectorAll(".moving-shape");  
+    if(shapes.length >9) {
+        return;
+      }
+    var shape = document.createElement('div');
+    shape.classList.add('moving-shape');
+    var width = Math.floor(Math.random() * (300 - 50 + 1)) + 50;
+    shape.style.width = width + 'px';
+    var right = Math.floor(Math.random() * (window.innerWidth * 0.9 - (-50) + 1)) + (-50);
+    shape.style.right = right + 'px';
+    var duration = Math.floor(Math.random() * (8 - 2 + 1)) + 2;
+    shape.style.animationDuration = duration + 's';
+    var randomColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+    shape.style.backgroundColor = randomColor;
+    document.body.appendChild(shape);
+    shape.addEventListener('animationend', function() {
+        let shapes = document.querySelectorAll(".moving-shape"); 
+        console.log(shapes.length);
+        shape.remove();
+        shapes = document.querySelectorAll(".moving-shape"); 
+        console.log(" -1 = " + shapes.length);
+    });
+}
+
+createMovingShape();
+setInterval(createMovingShape, 4000);
